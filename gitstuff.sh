3dad1e82da4a1c378f80cbf7d2c4b58f461c5aa2
JulienBe <bertozzijulien@gmail.com>
2016-04-25 16:44:33 +0200

    git stuff

---
+++ gitstuff.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global user.name JulienBe
+git config --global user.email bertozzijulien@gmail.com
+git config credential.helper 'cache --timeout=3600'
+git config --global core.editor vim
+
