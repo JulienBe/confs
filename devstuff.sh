354a30240f0bb5cf8fb2dd569c5e6520ba790100
JulienBe <bertozzijulien@gmail.com>
2016-05-05 07:17:58 +0200

    ruby & asciidoctor

---
+++ devstuff.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo pacman -S python-pip \¬
+jdk8-openjdk \¬
+intellij-idea-community-edition \¬
+ruby¬
+
+gem install asciidoctor
+
--- gitstuff.sh
+++ gitstuff.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Executing git config" 
+
 git config --global user.name JulienBe
 git config --global user.email bertozzijulien@gmail.com
 git config credential.helper 'cache --timeout=3600'
